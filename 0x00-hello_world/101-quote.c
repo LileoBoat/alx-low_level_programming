91f94e5adc0412be2a91c6c148d0de7a85eb074f
LileoBoat <galileoboateng@gmail.com>
2023-06-15 17:21:08 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
   * main - Entry point
   *
-  * Description: print a quoteusing write function
+  * Description: print a quote using write function
   *	write(int fd.const void *buf,size_t count)
   *
   * Return: 1 (not success)
@@ -14,3 +14,4 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 	return (0);
 }
+
